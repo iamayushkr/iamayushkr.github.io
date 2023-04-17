43b93d39ab46ea45a7a482cee351683008454615
Ayush Kumar <ak2210749@gmail.com>
2023-04-18 03:49:53 +0530

    Removed NavBar

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 $(document).ready(function() {
-     // add click event to navigation links
-     $('nav a').click(function(e) {
-       e.preventDefault();
-       var target = $(this).attr('href');
-       $('html, body').animate({
-         scrollTop: $(target).offset().top
-       }, 1000); // animate scroll to target section
-     });
-   }); 
+  var sections = $('section');
+  var index = 0;
+  var scrolling = false;
+
+  // Set the height of each section to the viewport height minus the height of the navigation bar
+  sections.height($(window).height() - $('nav').height());
+
+  $(window).resize(function() {
+    // Update the height of each section when the window is resized
+    sections.height($(window).height() - $('nav').height());
+  });
+
+  $(window).on('wheel', function(e) {
+    e.preventDefault();
+
+    // Only scroll if we're not already scrolling
+    if (!scrolling) {
+      scrolling = true;
+
+      // Determine the direction of the scroll (up or down)
+      var direction = (e.originalEvent.deltaY > 0) ? 1 : -1;
+
+      // Update the index of the current section
+      index += direction;
+      if (index < 0) {
+        index = 0;
+      } else if (index >= sections.length) {
+        index = sections.length - 1;
+      }
+
+      // Scroll to the next section
+      $('html, body').animate({
+        scrollTop: sections.eq(index).offset().top
+      }, 1, function() {
+        scrolling = false;
+      });
+    }
+  });
+});
+
